820251070ed6eb743e98096fad97032cdc6213dd
kakafe <sk2504@naver.com>
2019-01-29 21:36:05 +0900

    add) amamzon copy
    스크롤 이벤트 반응 구현 및 반응된 페이지에서의 인터렉션 구현을 test

--- amazoncopy/nav.js
+++
@@ -1,8 +0,0 @@
-let planpage = document.querySelector(".planpage");
-
-window.addEventListener('scroll', function () {
-	if (pageYOffset > 112) planpage.classList.add("planpage-scroll-middle");
-	if (pageYOffset < 112) planpage.classList.remove("planpage-scroll-middle");
-	if (pageYOffset > 465) planpage.classList.add("planpage-scroll-bottom");
-	if (pageYOffset < 465) planpage.classList.remove("planpage-scroll-bottom");
-});
