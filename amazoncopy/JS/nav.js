38c964a7f18f5a9b569ba60a2e287ec5964c0fa2
kakafe <sk2504@naver.com>
2019-02-22 16:09:15 +0900

    detail page markup 구현
    carousel 부분 마크업 구현및 fastdetail 마크업 까지 구현

--- amazoncopy/JS/nav.js
+++ amazoncopy/JS/nav.js
@@ -1,47 +1,58 @@
-class Nav_Elements {
-  constructor() {
-    this.planPage = document.querySelector(".planpage");
-    this.morePage = document.querySelector(".planpage-more-contents");
-    this.planPageCnt = document.querySelector(".planpage-contents");
-    this.moreCntBtn = document.querySelector(".more-contents-closebtn");
-    this.moreCntArrow = document.querySelector(".more-contents-arrow-img");
-    this.morePlanBtn = document.querySelector(".planpage-contents-txt");
-    this.navPosition = 112;
-    this.primBtnPosition = 465;
+class NavElements {
+  constructor(positionVal) {
+    this.plan = document.querySelector(".plan");
+    this.morePage = document.querySelector(".plan-more-contents");
+    this.planCnt = document.querySelector(".plan-contents");
+    this.navPosition = positionVal.navPosition;
+    this.primBtnPosition = positionVal.primBtnPosition;
   }
   removeClassList(el, className) {
     el.classList.remove(className);
   }
   removeClass() {
-    this.removeClassList(this.planPage, "planpage-more-click");
-    this.removeClassList(this.morePage, "planpage-more-click");
-    this.removeClassList(this.planPageCnt, "planpage-contents-downsize");
+    this.removeClassList(this.plan, classMap.click);
+    this.removeClassList(this.morePage, classMap.click);
+    this.removeClassList(this.planCnt, classMap.donwSize);
   }
   addMorePlanDownSizeEvent() {
-    this.moreCntBtn.addEventListener("click", this.removeClass.bind(this));
-    this.moreCntArrow.addEventListener("click", this.removeClass.bind(this));
+    const moreCntBtn = document.querySelector(".more-contents-closebtn");
+    const moreCntArrow = document.querySelector(".more-contents-arrow-img");
+    moreCntBtn.addEventListener("click", this.removeClass.bind(this));
+    moreCntArrow.addEventListener("click", this.removeClass.bind(this));
   }
   addScrollEvent() {
     window.addEventListener("scroll", this.scrollHandler.bind(this));
   }
   scrollHandler() {
-    const planClassList = this.planPage.classList;
-    if (pageYOffset > this.navPosition) planClassList.add("planpage-scroll-middle");
-    else planClassList.remove("planpage-scroll-middle");
-    if (pageYOffset > this.primBtnPosition) planClassList.add("planpage-scroll-bottom");
-    else planClassList.remove("planpage-scroll-bottom");
+    const planClassList = this.plan.classList;
+    if (pageYOffset > this.navPosition) planClassList.add(classMap.middleIntrc);
+    else planClassList.remove(classMap.middleIntrc);
+    if (pageYOffset > this.primBtnPosition)
+      planClassList.add(classMap.bottomIntrc);
+    else planClassList.remove(classMap.bottomIntrc);
   }
   addMoreClickEvent() {
-    this.morePlanBtn.addEventListener("click", this.addClass.bind(this));
+    const morePlanBtn = document.querySelector(".plan-contents-txt");
+    morePlanBtn.addEventListener("click", this.addClass.bind(this));
   }
   addClass() {
-    this.planPage.classList.add("planpage-more-click");
-    this.morePage.classList.add("planpage-more-click");
-    this.planPageCnt.classList.add("planpage-contents-downsize");
+    this.plan.classList.add(classMap.click);
+    this.morePage.classList.add(classMap.click);
+    this.planCnt.classList.add(classMap.donwSize);
+  }
+  interaction() {
+    this.addScrollEvent();
+    this.addMoreClickEvent();
+    this.addMorePlanDownSizeEvent();
   }
 }
 
-const NavElements = new Nav_Elements();
-NavElements.addScrollEvent();
-NavElements.addMoreClickEvent();
-NavElements.addMorePlanDownSizeEvent();
+const classMap = {
+  middleIntrc: "plan-scroll-middle",
+  bottomIntrc: "plan-scroll-bottom",
+  click: "plan-more-click",
+  donwSize: "plan-contents-downsize"
+};
+
+const navElement = new NavElements({navPosition : 112, primBtnPosition : 465});
+navElement.interaction();
