331abba4695fef7bbea49fe76f18748d56ae9165
kakafe <sk2504@naver.com>
2019-07-29 19:27:03 +0900

    무비부비  demo update
    readme 수정및 크롤링, 위치정보 기능 비활성화

--- movie_boovie_demo/js/crawling.js
+++ movie_boovie_demo/js/crawling.js
@@ -1,17 +1,17 @@
-var cheerio = require('cheerio');
-const request = require('request-promise')
+// var cheerio = require('cheerio');
+// const request = require('request-promise')
 
-var url = 'http://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%A1%AF%EB%8D%B0%EC%8B%9C%EB%84%A4%EB%A7%88+%EC%9A%A9%EC%82%B0%EC%A0%90'
+// var url = 'http://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%A1%AF%EB%8D%B0%EC%8B%9C%EB%84%A4%EB%A7%88+%EC%9A%A9%EC%82%B0%EC%A0%90'
 
 
-request(url).then(function(html) {
+// request(url).then(function(html) {
 
-    // Cheerio 오브젝트 생성
-    const $ = cheerio.load(html)
-        // 셀렉터 캐시로 Cheerio 오브젝트 생성
-    const $articleList = $('div')
-    console.log($.html())
-})
+//     // Cheerio 오브젝트 생성
+//     const $ = cheerio.load(html)
+//         // 셀렉터 캐시로 Cheerio 오브젝트 생성
+//     const $articleList = $('div')
+//     console.log($.html())
+// })
 
 
 // const express = require('express');
