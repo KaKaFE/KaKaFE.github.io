d8fe635cbae11030caf746e9d655ff64fed10b60
kakafe <sk2504@naver.com>
2019-08-20 23:13:13 +0900

    modify) 캐로셀 버튼정렬및 동작방식 수정

--- amazoncopy/JS/carousel.js
+++ amazoncopy/JS/carousel.js
@@ -6,21 +6,21 @@ class Carousel {
   constructor() {
     this.imgNum = 0;
     this.Width = imgWrapper.offsetWidth;
-    this.transitionVal = "transform 0.1s ease-in-out";
+    this.transitionVal = "transform 0.08s ease-in-out";
   }
 }
 
 const carousel = new Carousel();
 
 const leftMove = function() {
-  if (carousel.imgNum === 2) {
-    imgWrapper.style.transition = "transform 0.1s ease-in-out";
-    imgWrapper.style.transform = `translate3D(-${carousel.Width}px, 0, 0)`;
+  if (carousel.imgNum === 0) {
+    imgWrapper.style.transition = carousel.transitionVal
+    imgWrapper.style.transform = `translate3D(-${280}px, 0, 0)`;
   } else {
     carousel.imgNum++;
-    imgWrapper.style.transition = "transform 0.1s ease-in-out";
+    imgWrapper.style.transition = carousel.transitionVal
     carousel.Width += imgWrapper.offsetWidth;
-    imgWrapper.style.transform = `translate3D(-${carousel.Width}px, 0, 0)`;
+    imgWrapper.style.transform = `translate3D(-${280}px, 0, 0)`;
   }
 };
 
@@ -28,11 +28,11 @@ const rightMove = function() {
   if (carousel.imgNum > 0) {
     carousel.imgNum--;
     carousel.Width -= imgWrapper.offsetWidth;
-    imgWrapper.style.transition = "transform 0.1s ease-in-out";
+    imgWrapper.style.transition = carousel.transitionVal
     imgWrapper.style.transform = `translate3D(-${carousel.Width - imgWrapper.offsetWidth}px, 0, 0)`;
   } else {
-    imgWrapper.style.transition = "transform 0.1s ease-in-out";
-    imgWrapper.style.transform = `translate3D(${0}px, 0, 0)`;
+    imgWrapper.style.transition = carousel.transitionVal
+    imgWrapper.style.transform = `translate3D(${280}px, 0, 0)`;
   }
 };
 
@@ -41,16 +41,16 @@ const moveimg = function() {
   imgWrapper.insertBefore(
     imgWrapper.lastElementChild.cloneNode(true),
     imgWrapper.firstElementChild
-  );
-  imgWrapper.removeChild(imgWrapper.lastElementChild);
-  imgWrapper.style.transform = `translate3D(-280px,0,0)`;
+    );
+    imgWrapper.removeChild(imgWrapper.lastElementChild);
+    imgWrapper.style.transform = `translate3D(0px,0,0)`;
 };
 
 const reverseImg = function() {
   imgWrapper.style.transition = "none";
   imgWrapper.appendChild(imgWrapper.firstElementChild.cloneNode(true));
   imgWrapper.removeChild(imgWrapper.firstElementChild);
-  imgWrapper.style.transform = `translate3D(-560px,0,0)`;
+  imgWrapper.style.transform = `translate3D(-280px,0,0)`;
 };
 
 leftArr.addEventListener("click", rightMove);
